2e24cbcffa423a3254fe12f7e6a958331697b8ef
prasanth <prasanth8142@gmail.com>
2025-12-10 17:40:16 +0530

    DOM-Methods

---
+++ JavaScript/Training/10-12-2025/DomMethod.js
@@ -0,0 +1,33 @@
+// let title=document.getElementById('mainTitle')
+
+console.log(document)
+let h1=document.getElementById('header')
+console.log(h1)
+let h2=document.getElementById('nano')
+console.log(h2)
+let h3=document.getElementsByClassName('title')
+console.log(h3)
+let h4=document.getElementsByTagName('h2')
+console.log(h4)
+let h5=document.querySelector(".h1")
+console.log(h5)
+let h6=document.querySelectorAll("h2")
+console.log(h6)
+
+let m=document.getElementById('madhu')
+console.log(m)
+
+
+let n=document.getElementsByClassName('sai')
+console.log(n)
+
+let x=document.getElementsByTagName('h6')
+console.log(x)
+// let y=document.querySelector(h8)
+// console.log(y)
+
+let D=document.querySelector("p")
+console.log(D)
+
+let undeclare=document.querySelector('a')
+console.log(undeclare)
