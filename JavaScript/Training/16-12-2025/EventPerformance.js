6a886e5568b14a0f606d03f3dc5df4710231a15e
prasanth <prasanth8142@gmail.com>
2025-12-18 17:48:37 +0530

    PromiseMethods

--- JavaScript/Training/16-12-2025/EventPerformance.js
+++ JavaScript/Training/16-12-2025/EventPerformance.js
@@ -1,59 +1,61 @@
-let button=document.getElementById('button')
+// let button=document.getElementById('button')
 
-let a=0
-button.addEventListener('click',function(){
-    a+=1
-    console.log("value of a",a)
-console.log('button clicked')
-})
+// let a=0
+// button.addEventListener('click',function(){
+//     a+=1
+//     console.log("value of a",a)
+// console.log('button clicked')
+// })
 
-let b=0
-let lastclick=0
+// let b=0
+// let lastclick=0
 
-button.addEventListener('click',function(){
-    let present=new Date()
-    if(present-lastclick>2000){
-        console.log('value of b is',b)
-        b+=1
-        lastclick=present
-    }
-    // console.log('button clicked')
-})
+// button.addEventListener('click',function(){
+//     let present=new Date()
+//     if(present-lastclick>2000){
+//         console.log('value of b is',b)
+//         b+=1
+//         lastclick=present
+//     }
+//     // console.log('button clicked')
+// })
 
-let c=0
-let timer
-button.addEventListener('click',function(){
-    clearTimeout(timer)
-    timer=setTimeout(() => {
-        c+=1
-        console.log('value of c is',c)
-    }, 2000);
-})
-
-
-// call back function
+// let c=0
+// let timer
+// button.addEventListener('click',function(){
+//     clearTimeout(timer)
+//     timer=setTimeout(() => {
+//         c+=1
+//         console.log('value of c is',c)
+//     }, 2000);
+// })
 
 
-function sum(s){
-console.log(s)
-}
-function name(a,b,c){
-    console.log('Inner function')
-c(a+b)
-}
-name(1,2,sum)
-let list=document.getElementById('list')
-list.addEventListener('click',(g)=>{
-console.log(g.target)
-})
+// // call back function
 
 
-let n=new Promise((reso,reject)=>{
-let status=true
-if(status){
-    resolve('true')
-}
-else{
-    reject('false')
-}
-})
+// function sum(s){
+// console.log(s)
+// }
+// function name(a,b,c){
+//     console.log('Inner function')
+// c(a+b)
+// }
+// name(1,2,sum)
+// let list=document.getElementById('list')
+// list.addEventListener('click',(g)=>{
+// console.log(g.target)
+// })
+
+
+// let n=new Promise((reso,reject)=>{
+// let status=true
+// if(status){
+//     resolve('true')
+// }
+// else{
+//     reject('false')
+// }
+// })
+
+// console.log(Math.minLargest(10,20,30))
---
+++ JavaScript/Training/17-12-2025/Pending.js
@@ -0,0 +1,104 @@
+// HelloWord
+// HELLO-WORD
+// 19. String to Capitalized Dash Array
+// Description: Write a function that converts the string "Hello World" into an array of words, capitalizes each word, and joins them with a dash.
+// Expected Output:
+
+
+// function capitalize(str) {
+//  return str.toUpperCase().replace(" ", "-");
+// }
+// console.log(capitalize("Hello World")); 
+
+
+// function array(str){
+//   let array=[str]
+//  return str.toUpperCase()
+// }
+
+
+// console.log(array('hello word'))
+
+
+
+// 7. Season Finder
+// Description: Write a function season(month) that returns the season based on the month number (1–12). Use a switch-case statement.
+// •	Winter: December, January, February
+// •	Spring: March, April, May
+// •	Summer: June, July, August
+// •	Autumn: September, October, November
+// let obj={january:1,feb:2,march:3,april:4,may:5,june:6,july:7,aug:8,sep:9,oct:10,nov:11,dec:12}
+// let m=[]
+//  for(x in obj){
+//     m=obj[x]
+//  }
+// function season(m){
+    
+// switch(m){
+//     case "12","1","2":
+//         console.log("winter")
+//         break;
+//         case 3,4,5:
+//             console.log("Spring")
+//             break
+//             case 6,7,8:
+//                 console.log("Summer")
+//                 break
+//                 case 9,10,11:
+//                 console.log("Autumn")
+//                 break
+//                 default:
+//                     console.log("out of month")
+// }
+//     }
+
+// season("1")
+
+// function num(a){
+//     switch(a){
+//         case "january","Feburay","March":
+//             console.log("winter:")
+//             break
+//             case "April","May",'Sep':
+//                 console.log("summer")
+//                 break
+//                 case 3:
+//                     console.log("autum")
+//     }
+// }
+// num(6)
+
+
+// function prime(num){
+//     if(num<=1){
+//         return false
+//     }
+//     for(let i=2;i<num;i++){
+//         if(num%2===0){
+//            return false
+       
+//     }
+// }
+//    return true 
+// }
+// console.log(prime(17))
+
+
+
+// function even(t){
+//     for(let i=1;i<=t;i++){
+//         if(i%2==0){
+//             console.log(i)
+//         }
+//         // else{
+//         //     console.log(i)
+//         // }
+//     }
+// }
+// even(5)
+
+
+// let p1=new Promise(resolve)
+// p1.PromiseAll(()=>{
+//     console
+// })
