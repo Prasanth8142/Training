3988e620b1f390d139c6b8c558e1566337971770
prasanth <prasanth8142@gmail.com>
2025-12-26 17:44:44 +0530

    OutputMethods

---
+++ JavaScript/Training/26-12-2025/OutputMethods.js
@@ -0,0 +1,7 @@
+// let a=prompt()
+// console.log(a)
+
+console.log(innerText="Hai")
+document.write("hai")
+let b=alert("Warning")
+document.write()
---
+++ JavaScript/Training/26-12-2025/Typeconversions.js
@@ -0,0 +1,46 @@
+let a="1234.234"
+let b=124
+console.log(typeof a)
+let conversion=parseInt(a)
+console.log( typeof conversion)
+let c1=toString(b)
+console.log(typeof c1)
+
+let c=123.4567
+console.log(parseFloat(c))
+console.log(c.toFixed(2))
+
+
+let name="Prashanth"
+console.log(name.toLowerCase())
+console.log(name.toUpperCase())
+
+console.log("Output Methods")
+
+alert("Hello User!") 
+
+document.write("Welcome Back User")
+
+// let a = document.getElementById("h1")
+// console.log(a)
+
+// let b = document.getElementById("para")
+// console.log(b)
+
+//BOM(Browser Object Model)
+
+console.log(screen.width)
+
+console.log(screen.height)
+
+console.log(navigator.userAgent)
+
+console.log("Common Methods")
+
+let data= prompt("Enter the value")
+let m = parseInt(data)
+console.log(typeof(m))
+console.log(typeof(m.toString()))
+
+let f = 10.342534346
+console.log(f.toFixed(3))
