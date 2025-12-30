304696b6c1676a459de756a5bc496d964b137cca
prasanth <prasanth8142@gmail.com>
2025-12-30 17:55:28 +0530

    Git

---
+++ JavaScript/Training/30-12-2025/GitIntro.js
@@ -0,0 +1,15 @@
+console.log("Intro to Git and GitHub")
+console.log("Git:- Git is a version control system.")//tool
+console.log("It helps you track changes in your code over time.")
+
+console.log("GitHub:- GitHub is a cloud platform that stores Git repositories online.")
+//services that hosts git projects
+
+console.log("Git Commands")
+console.log("git init")         
+console.log("git status")   
+console.log("git commit -m messsage")     
+console.log("git add .")         
+console.log("git branch")        
+console.log("git push")          
+console.log("git pull")
