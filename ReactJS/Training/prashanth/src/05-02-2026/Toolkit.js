6c0b70e0b99126610d78993dd09a83eca7f46c58
prasanth <prasanth8142@gmail.com>
2026-02-05 18:14:55 +0530

    Intro of ReduxToolkit

---
+++ ReactJS/Training/prashanth/src/05-02-2026/Toolkit.js
@@ -0,0 +1,26 @@
+// Multiple files per feature
+
+// Lots of action types and switch statements
+
+// Manual immutable updates
+
+// 1. configureStore
+
+// Replaces createStore.
+// It automatically:
+
+// Combines reducers
+
+// Adds Redux DevTools
+
+// Adds middleware like redux-thunk
+// 2. createSlice
+
+// The heart of RTK.
+// A slice combines:
+
+// State
+
+// Reducers
+
+// Actions
