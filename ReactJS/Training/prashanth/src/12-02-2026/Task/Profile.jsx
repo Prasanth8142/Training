e2b811fc73d7a0763a470b6f60f54a14550fc7b8
prasanth <prasanth8142@gmail.com>
2026-02-12 18:31:41 +0530

    ConditionalRendering

---
+++ ReactJS/Training/prashanth/src/12-02-2026/Task/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+function Profile({ role, setPage }) {
+  return (
+    <div>
+      <h1>{role} Profile Page</h1>
+
+      <button onClick={() => setPage("home")}>
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Profile;
--- ReactJS/Training/prashanth/src/App.jsx
+++ ReactJS/Training/prashanth/src/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Reducer from './11-02-2026/Reducer'
+import { useState } from "react";
+import Home from './12-02-2026/Task/Home'
+
+import Profile from './12-02-2026/Task/Profile'
+import Login from "./12-02-2026/Task/Login"
 
 function App() {
+  const [page, setPage] = useState("home");   
+  const [role, setRole] = useState("");
+
   return (
     <div>
+      {page === "home" && (
+        <Home setPage={setPage} setRole={setRole} />
+      )}
 
-      <Reducer/>
+      {page === "login" && (
+        <Login role={role} setPage={setPage} />
+      )}
+
+      {page === "profile" && (
+        <Profile role={role} setPage={setPage} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
