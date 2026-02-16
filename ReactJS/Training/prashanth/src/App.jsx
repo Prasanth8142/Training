f97f94722927c5d0eb4ba3ef9f9170ff4ae1e638
prasanth <prasanth8142@gmail.com>
2026-02-16 17:38:12 +0530

    Data-binding

--- ReactJS/Training/prashanth/src/App.jsx
+++ ReactJS/Training/prashanth/src/App.jsx
@@ -1,30 +1,14 @@
-import { useState } from "react";
-import Home from './12-02-2026/Task/Home'
-
-import Profile from './12-02-2026/Task/Profile'
-import Login from "./12-02-2026/Task/Login"
-import Boy from "./13-02-2026/Boy";
+import React from 'react'
+import DataBinding from './16-02-2026/Databinding'
+import Login from './16-02-2026/Login'
 
 function App() {
-  const [page, setPage] = useState("home");   
-  const [role, setRole] = useState("");
-
   return (
     <div>
-      {/* {page === "home" && (
-        <Home setPage={setPage} setRole={setRole} />
-      )}
-
-      {page === "login" && (
-        <Login role={role} setPage={setPage} />
-      )}
-
-      {page === "profile" && (
-        <Profile role={role} setPage={setPage} />
-      )} */
-      }<Boy/>
+      <DataBinding/>
+      <Login/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
