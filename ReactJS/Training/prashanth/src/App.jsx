192be4b293c15ed9b142c34f6cb62b748c80df62
prasanth <prasanth8142@gmail.com>
2026-03-17 17:16:04 +0530

    Question Paper practice

--- ReactJS/Training/prashanth/src/App.jsx
+++ ReactJS/Training/prashanth/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import C from './03-03-2026/C'
-import Boy from './13-02-2026/Boy'
+import A from './17-03-2026/A'
 // import A from './03-03-2026/A'
 
 function App() {
   return (
     <div>
-   <Boy/>
+   <A/>
     </div>
   )
 }
