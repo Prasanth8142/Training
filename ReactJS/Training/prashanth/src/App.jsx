84f452cddcdeedad47713f05cf2466dfa587265d
prasanth <prasanth8142@gmail.com>
2026-03-25 17:47:35 +0530

    Redux

--- ReactJS/Training/prashanth/src/App.jsx
+++ ReactJS/Training/prashanth/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
-import A from './24-03-2026/A'
+import { store } from './25-03-2026/B'
+import { Provider } from 'react-redux'
+import A from './25-03-2026/A'
 function App() {
   return (
-    <div>
-      <A/>
+    <div >
+      <Provider store={store}>
+        <A/>
+      </Provider>
+      
     </div>
   )
 }
