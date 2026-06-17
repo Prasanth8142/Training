ee3f1de2c4f1bb81e6999799df932e5c0aa1fc86
prasanth <prasanth8142@gmail.com>
2026-06-17 18:54:18 +0530

    Revision

---
+++ JavaScript/Training/17-06-2026/OutputMethods/InnerText/A.js
@@ -0,0 +1,4 @@
+document.getElementById("demo").innerText = "Hello World"
+console.log(document.getElementById("demo"))
+document.getElementById("output").innerText = "This is a preformatted text block."
+console.log(document.getElementById("output"))
---
+++ JavaScript/Training/17-06-2026/OutputMethods/innerHTML/A.js
@@ -0,0 +1,6 @@
+let a=document.getElementById("demo")
+a.innerHTML="Hello World"
+console.log(a)
+
+
+document.getElementById("output").innerHTML="This is a preformatted text block."
